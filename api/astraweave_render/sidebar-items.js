2b22a4dd90bc6f0ce28ff4751ac950bf79d262ed
lazyxeon <79929721+lazyxeon@users.noreply.github.com>
2025-09-14 20:23:46 +0000

    Deploying to gh-pages from @ lazyxeon/AstraWeave-AI-Native-Gaming-Engine@b8a5e2ff89244ba218fd7ce25200fa224dd57812 🚀

--- api/astraweave_render/sidebar-items.js
+++ api/astraweave_render/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"mod":["camera","depth","effects","overlay","primitives","renderer","texture","types"]};
+window.SIDEBAR_ITEMS = {"mod":["camera","depth","effects","overlay","primitives","renderer","terrain","texture","types"]};
