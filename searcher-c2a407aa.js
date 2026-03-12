6e125f6661d833f29d0efd1eb02845fbfb49f02a
lazyxeon <lazyxeon@users.noreply.github.com>
2026-03-12 20:56:25 +0000

    Deploy mdBook documentation: d97d600b71e9d4030e5d0b9844c22cac142dacb1 d97d600b71e9d4030e5d0b9844c22cac142dacb1

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-9fbab890.js',
+                path_to_root + 'searchindex-3b1fa130.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
